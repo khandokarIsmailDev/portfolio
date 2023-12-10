bbfc9e16ab2cf685ec133d0a6575aa491cd23c6c
khandokarIsmailDev <ismailkhandokar000@gmail.com>
2023-12-10 21:40:37 +0600

    update .gitignore file

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"wi35dLMvvTukxvw4YZqB17Sm+zorTsAkL/Bhc261ksg=\"}"
---
+++ .next/static/chunks/app/services/layout-65a7bd8419c75bcc.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[267,555,931,301,841,506,327,319,462,988,269,469,752,925,239,495,461,806,599,486,596],{3521:function(){}},function(n){n.O(0,[971,472,744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
